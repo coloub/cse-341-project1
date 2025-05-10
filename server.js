5f856c628d5831836e47d0c7ef54e260b177b3e7
coloub <jose1252jose@gmail.com>
2025-05-10 11:49:10 -0400

    #

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
 
-const express = require('express')
+
+const express = require('express');
 
 const mongodb = require('./data/database');
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(`/`, require(`./routes`));
 
@@ -18,5 +18,5 @@ mongodb.initDb((err)=>{
 
     }
 
-})
+});
 
