690d5469baa7fed88a32f3a57378e8aa457d037e
coloub <jose1252jose@gmail.com>
2025-05-10 13:02:06 -0400

    #

--- data/database.js
+++ data/database.js
@@ -1,5 +1,6 @@
 const dotenv =require('dotenv')
 dotenv.config();
+console.log('Loaded MONGODB_URL:', process.env.MONGODB_URL);
 
 const MongoClient = require('mongodb').MongoClient;
 
@@ -10,7 +11,12 @@ const initDb = (callback) => {
         console.log('Db is already initialized!');
         return callback(null, database);
     }
-    MongoClient.connect (process.env.MONGODB_URL)
+    if (!process.env.MONGODB_URL) {
+        const errMsg = 'Error: MONGODB_URL environment variable is not set. Please set it in your .env file or environment.';
+        console.error(errMsg);
+        return callback(new Error(errMsg));
+    }
+    MongoClient.connect(process.env.MONGODB_URL)
     .then((client)=>{
         database = client;
         callback(null, database);
